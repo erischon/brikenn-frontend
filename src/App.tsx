416620cb15e93e58ec61cd4a3cd4776eaaf9f80a
Eri Schon <erischon@gmail.com>
2023-02-02 12:55:28 +0100

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Landing from "./pages/Landing";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Brikenn</h1>
+      <Landing />
     </div>
   );
 }
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,8 @@
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+
+const Landing = () => {
+  return <div>Landing</div>;
+};
+
+export default Landing;
